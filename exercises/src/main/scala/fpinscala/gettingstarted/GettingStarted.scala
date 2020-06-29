1872bbb99e1d4c9c08405a8f86ff85e60a2faae7
Ivan <vandyachen@gmail.com>
2020-06-29 20:06:27 +0300

    Getting started with FP
    - [x] Exercise 2.2: Implement `isSorted`, which check whether an `Array[A]` is sorted...

--- exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -147,7 +147,9 @@ object PolymorphicFunctions {
 
   // Exercise 2: Implement a polymorphic function to check whether
   // an `Array[A]` is sorted
-  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = ???
+  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean =
+    if (as.length < 2) true
+    else as.zip(as.tail).forall(gt.tupled)
 
   // Polymorphic functions are often so constrained by their type
   // that they only have one implementation! Here's an example:
