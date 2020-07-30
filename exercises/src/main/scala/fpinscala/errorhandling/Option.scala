bad872794951f19920a21c79b08f17911011b635
Ivan Dyachenko <vandyachen@gmail.com>
2020-07-31 02:20:24 +0300

    Handling errors without exceptions
    - [x] Exercise 4.2: Implement the `variance` function in terms of `flatMap`.
    - [x] Exercise 4.3: Write a generic function `map2` that combines two `Option` values...
    - [x] Exercise 4.4: Write a function `sequence` that combines a list of `Option`s into one `Option`...

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -51,11 +51,33 @@ object Option {
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
-  def variance(xs: Seq[Double]): Option[Double] = ???
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = ???
+  /**
+    * Exercise 4.2
+    * Implement the `variance` function in terms of `flatMap`.
+    */
+  def variance(xs: Seq[Double]): Option[Double] =
+    mean(xs).map(m => xs.map(x => math.pow(x - m, 2))).flatMap(mean)
 
-  def sequence[A](a: List[Option[A]]): Option[List[A]] = ???
+  /**
+    * Exercise 4.3
+    * Write a generic function `map2` that combines two `Option` values using a binary func-
+    * tion. If either `Option` value is `None`, then the return value is too.
+    */
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    a.flatMap(x => b.map(f(x, _)))
+
+  /**
+    * Exercise 4.4
+    * Write a function `sequence` that combines a list of `Option`s into one `Option` containing
+    * a list of all the `Some` values in the original list. If the original list contains `None` even
+    * once, the result of the function should be `None`; otherwise the result should be `Some` with
+    * a list of all the values.
+    */
+  def sequence[A](a: List[Option[A]]): Option[List[A]] =
+    a.foldLeft[Option[List[A]]](Some(Nil)) { (xsOpt, xOpt) =>
+      xsOpt.flatMap(xs => xOpt.map(x => x :: xs))
+    }.map(_.reverse)
 
   def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = ???
 }
