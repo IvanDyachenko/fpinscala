00c3c012309c8447f027cb90a3fb5fa9559bbf5e
Ivan Dyachenko <vandyachen@gmail.com>
2020-07-31 15:30:19 +0300

    Handling errors without exceptions
    - [x] Exercise 4.6: Implement versions of `map`, `flatMap`, `orElse`, and `map2` on `Either`...

--- exercises/src/main/scala/fpinscala/errorhandling/Either.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Either.scala
@@ -1,16 +1,31 @@
 package fpinscala.errorhandling
 
-
 import scala.{Option => _, Either => _, Left => _, Right => _, _} // hide std library `Option` and `Either`, since we are writing our own in this chapter
 
-sealed trait Either[+E,+A] {
- def map[B](f: A => B): Either[E, B] = ???
+/**
+  * Exercise 4.6
+  * Implement versions of `map`, `flatMap`, `orElse`, and `map2` on `Either` that operate in the
+  * `Right` value.
+  */
+sealed trait Either[+E, +A] {
 
- def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = ???
+  def map[B](f: A => B): Either[E, B] = this match {
+    case Right(a) => Right(f(a))
+    case Left(e)  => Left(e)
+  }
 
- def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = ???
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+    case Left(e)  => Left(e)
+    case Right(a) => f(a)
+ }
 
- def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = ???
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+    case Left(e)  => b
+    case Right(a) => Right(a)
+  }
+
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+    for { ra <- this; rb <- b } yield f(ra, rb) //this.flatMap(a => b.map(f(a, _)))
 }
 case class Left[+E](get: E) extends Either[E,Nothing]
 case class Right[+A](get: A) extends Either[Nothing,A]
